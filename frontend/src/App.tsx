acea0ea8c82c53e433352932d932cfb756c56ab4
ZhaoYuLL <zhaoyu0305@gmail.com>
2024-08-27 21:26:43 -0400

    set up pre commit and .prettierc

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BirdGuessingGame from "./components/BirdGuessingGame";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <BirdGuessingGame />
+    </div>
+  );
+};
+
+export default App;
