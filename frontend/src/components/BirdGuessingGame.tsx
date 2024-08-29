792de40e5f3b408bd15940515f4fea6d9dd2c2ff
ZhaoYuLL <zhaoyu0305@gmail.com>
2024-08-28 20:10:27 -0400

    setup basic keys from pulling the api

--- frontend/src/components/BirdGuessingGame.tsx
+++ frontend/src/components/BirdGuessingGame.tsx
@@ -1,1 +1,12 @@
-console.log("hi");
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+interface BirdRecording {
+  id: string;
+  en: string;
+  gen: string;
+  sp: string;
+  cnt: string;
+  loc: string;
+  audioUrl: string;
+}
