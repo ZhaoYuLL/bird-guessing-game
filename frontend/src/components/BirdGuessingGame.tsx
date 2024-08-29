f271c9cb8b5841b602e4cad3de8697686cdee038
ZhaoYuLL <zhaoyu0305@gmail.com>
2024-08-28 20:10:46 -0400

    ran formatter

--- frontend/src/components/BirdGuessingGame.tsx
+++ frontend/src/components/BirdGuessingGame.tsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 interface BirdRecording {
-  id: string;
-  en: string;
-  gen: string;
-  sp: string;
-  cnt: string;
-  loc: string;
-  audioUrl: string;
+    id: string;
+    en: string;
+    gen: string;
+    sp: string;
+    cnt: string;
+    loc: string;
+    audioUrl: string;
 }
