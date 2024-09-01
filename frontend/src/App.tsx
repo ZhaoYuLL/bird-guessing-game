7e818e7737d35d73ef13b871d18825f60526a726
ZhaoYuLL <58865189+ZhaoYuLL@users.noreply.github.com>
2024-09-01 14:39:03 -0400

    added tailwind to the guessing game

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,10 @@ import BirdGuessingGame from "./components/BirdGuessingGame";
 
 const App: React.FC = () => {
     return (
-        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-            <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+        <div className="min-h-screen bg-white-100 w-full flex items-center justify-center">
+            <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-lg">
                 <h1 className="text-3xl font-bold mb-6 text-center">
-                    Bird Guessing Game title
+                    Bird Guessing Game
                 </h1>
                 <BirdGuessingGame />
             </div>
