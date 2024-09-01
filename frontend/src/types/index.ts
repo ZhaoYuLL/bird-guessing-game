f45df7e7a0dc27d1ceeae1e5048c6d9e325948d2
ZhaoYuLL <zhaoyu0305@gmail.com>
2024-08-31 23:06:01 -0400

    updated file name

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,10 @@
+export interface BirdData {
+    id: string;
+    scientificName: string;
+    commonName: string;
+    recordingUrl: string;
+    location: string;
+    country: string;
+    recordist: string;
+    length: string;
+  }
